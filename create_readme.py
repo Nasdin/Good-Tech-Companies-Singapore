2f0c9c8c73eb08a4ec329d04e7857dd0f0fcea34
Nasrudin Bin Salim <nasrudin.salim@cartrack.com>
2019-12-26 15:14:19 +0800

    Added GoJek, Criteo

--- create_readme.py
+++ create_readme.py
@@ -27,8 +27,8 @@ automatically [regenerate](create_readme.py) this [README](README.md) file.
 
 ### To contribute:
 1. Fork repository
-2. Edit config.yaml 
-3. Run create_readme.py, which will generate new README.md
+2. Edit [config.yaml](config.yaml)
+3. Run [create_readme.py](create_readme.py), which will generate new README.md
 4. Open a pull request!
 
 
@@ -41,6 +41,17 @@ for personal appointments without consuming leaves. Or to work at home on any mo
     
 >Someone who is in their mid to late 20s. Past the junior phase and has 2-4 years experience.
 But 5 years and above can command a much higher salary; they would have "senior" or "lead" in their titles.
+
+3. __Striked through__
+
+>This company has fallen below certain standards and is in the grey area of a good tier tech company
+
+4. __Very good pantry__
+
+> Generally means a never ending flowing pantry, topped up daily with premium ice cream of any kind, drinks, bars, 
+magnum ice-cream, free flow alcohol. Milo, coconut water etc. Non-junk snacks, Good would be similar but slightly lower. 
+Use your imagination to figure out the other ranges.
+
 
 '''
 
